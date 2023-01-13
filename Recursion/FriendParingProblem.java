b7f523819211b06807791d3121cac40304024bbd
Yash Manmode <yashmanmode.official@gmail.com>
2023-01-13 10:12:15 +0530

    Add files via upload

--- Recursion/FriendParingProblem.java
+++ Recursion/FriendParingProblem.java
@@ -1,3 +1,15 @@
 public class FriendParingProblem {
+
+    public static int Friend_Pairing(int n) {
+        if (n == 1 || n == 2)
+            return n;
+
+        int total_ways = Friend_Pairing(n-1) + (n-1) * Friend_Pairing(n-2);
+        return total_ways;
+    }
     
+    public static void main(String[] args) {
+        int n = 5 ;
+        System.out.println(Friend_Pairing(n));
+    }
 }
