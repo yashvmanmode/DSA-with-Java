950d0aa43bbdb7350103541c1356ca4626f5e7b3
Yash Manmode <yashmanmode.official@gmail.com>
2023-01-22 09:41:15 +0530

    Add files via upload

---
+++ Backtracking/backtrack_array.java
@@ -0,0 +1,28 @@
+public class backtrack_array {
+
+    public static void changeArrVal(int arr[], int idx, int val) {
+
+        //base
+        if (idx == arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursive work
+        arr[idx] = val;
+        changeArrVal(arr, idx+1, val+1);
+        arr[idx] = arr[idx]-2;
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = new int[10];
+        changeArrVal(arr, 0, 1);
+        printArr(arr);
+    }
+}
