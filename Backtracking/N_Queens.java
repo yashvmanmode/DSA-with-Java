532612a39b387195e95e88e36fab2a88d6ba5b1f
Yash Manmode <yashmanmode.official@gmail.com>
2023-01-26 07:59:26 +0530

    Updated N_Queens Problem

--- Backtracking/N_Queens.java
+++ Backtracking/N_Queens.java
@@ -1,13 +1,23 @@
 public class N_Queens {
 
     public static void N_Queens_problem(char ChessBoard[][], int startRow) {
-        
+        if (startRow == ChessBoard.length) {
+            print(ChessBoard);
+            return;
+        }
+
+        for (int i=0; i<ChessBoard.length; i++) {
+            ChessBoard[startRow][i] = 'Q';
+            N_Queens_problem(ChessBoard, startRow+1);
+            System.out.println();
+            ChessBoard[startRow][i] = '_';
+        }
     }
 
-    public static void print(char ChessBoard[][], int n) {
+    public static void print(char ChessBoard[][]) {
 
-        for(int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
+        for(int i=0; i<ChessBoard.length; i++) {
+            for (int j=0; j<ChessBoard.length; j++) {
                 System.out.print(ChessBoard[i][j] + " ");
             }
             System.out.println();
@@ -15,7 +25,7 @@ public class N_Queens {
         
     }
     public static void main(String[] args) {
-        int n = 12;
+        int n = 2;
         char ChessBoard[][] = new char[n][n];
 
         //initialisation
