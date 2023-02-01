c00d482232b33df2116cb7bede5ad859051288f4
Yash Manmode <yashmanmode.official@gmail.com>
2023-02-01 08:28:20 +0530

    Add files via upload

--- ArrayList/Pairsum.java
+++ ArrayList/Pairsum.java
@@ -17,18 +17,39 @@ public class Pairsum {
         return false;
     }
 
+    public static boolean Pair_Sum_Optimised(ArrayList<Integer> arr, int target) {
+        
+        int leftPtr = 0;
+        int rightPtr = arr.size()-1;
+
+        while (leftPtr < rightPtr) {
+            int pairSum = arr.get(leftPtr) + arr.get(rightPtr);
+            if (pairSum == target)
+                return true;
+
+            else if (pairSum < target)
+                leftPtr ++;
+
+            else
+                rightPtr--;
+        }
+
+        return false;
+
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(8);
-        arr.add(6);
+        arr.add(1);
         arr.add(2);
-        arr.add(5);
-        arr.add(4);
-        arr.add(8);
         arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
         arr.add(7);
-        int target = 727;
+        arr.add(8);
+        int target = 1872;
 
-        System.out.println(Pair_Sum(arr, target));
+        System.out.println(Pair_Sum_Optimised(arr, target));
     }
 }
