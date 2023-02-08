07ccf338a7c05d7b00515ab339fa99064201c0f3
Yash Manmode <yashmanmode.official@gmail.com>
2023-02-08 09:12:57 +0530

    Add files via upload

---
+++ LinkedList/RemoveCycle.java
@@ -0,0 +1,106 @@
+public class RemoveCycle {
+
+    public static class Node { 
+        int data;
+        Node next;
+
+        public Node(int Data) {
+            data = Data;
+            this.next = null;
+        }
+       
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addLastLL(int Data) {
+        Node newNode = new Node(Data);
+
+        if (head == null){
+            head = tail = newNode;
+            size++;
+            return;
+        }
+        tail.next = newNode;
+        size++;
+        tail = newNode;
+    }
+
+    public void printLL() {
+
+        Node temp = head;
+        while (temp != null) {
+
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+
+    }
+
+
+    public static boolean DetectCycleinLL() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+
+    public static void remove_cycleLL() {
+        // detect cycle 
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+
+        if (cycle == false) 
+            return;
+
+        // find meeting point
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //remove cycle
+        prev.next = null;
+    }
+
+    
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next = head;        
+
+        System.out.println(DetectCycleinLL());
+        remove_cycleLL();
+        System.out.println(DetectCycleinLL());
+        
+    }
+}
