cda4f147566be7056563afbebb75377f0cedf4d3
Yash Manmode <yashmanmode.official@gmail.com>
2023-02-16 08:00:55 +0530

    Add files via upload

---
+++ Stack/implementationWithLL.java
@@ -0,0 +1,67 @@
+// Amazon, OYO
+
+public class implementationWithLL {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node temp = new Node(data);
+
+            if (head == null) {
+                head = temp;
+                return;
+            }
+
+            temp.next = head;
+            head = temp;
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+
+        while (!s.isEmpty()) {
+            System.out.println("Peek Element is :" + s.peek());
+            s.pop();
+        }        
+    }
+}
