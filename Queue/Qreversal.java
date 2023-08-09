14db88583685a0d5271febffb57b5f32e256f0a5
Yash Manmode <yashmanmode.official@gmail.com>
2023-08-09 20:09:31 +0530

    UPDATE

---
+++ Queue/Qreversal.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class Qreversal {
+
+    public static void reversal(Queue<Integer> q) {
+
+        Stack<Integer> s = new Stack<>();
+
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+
+        while(!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        reversal(q);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
+    }
+}
